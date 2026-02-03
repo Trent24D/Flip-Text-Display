a56255791a65a716ca7ba33f4ab432c7e0088c5b
Trent24D <trentandave1@gmail.com>
2026-02-03 15:31:08 -0800

    making flip_panel class

--- flip_panel.py
+++ flip_panel.py
@@ -1,23 +1,43 @@
-class fli_panel:
+import time, sys
 
-    def __init__(self, width, height):
-        self.curr = 0
-        self.characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+class flip_panel:
+
+    ALL_CHARACTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
                            'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
                            'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6',
                            '7', '8', '9', '!', '@', '#', '$', '%', '^', '&', '*', '(',
                            ')', '-', '_', '=', '+', '[', ']', '{', '}', ';', ':', "'"]
-        
-        def display(self):
-            print(self.characters[self.curr])
-        
-        def flip(self, search):
-            self.curr = self.change(self.curr)
-        
-        def change(self, index):
-            if index + 1 >= len(self.characters):
-                return 0
-            else:
-                return index + 1
+
+    def __init__(self, current_index=0, target_index=0, flip_duration=3.0):
+        self.current_index = 0
+        self.target_index = 0
+        self.duration = flip_duration
+
+        self.flips_needed = self.get_flips_needed()
+        self.seconds_per_flip = self.get_seconds_per_flip()
+
+        self.last_flip = time.time()
+
+
+    def get_flips_needed(self):
+
+        return (self.target_index - self.current_index) % len(self.ALL_CHARACTERS)
+    
+    def get_seconds_per_flip(self):
+        return self.duration / self.flips_needed if self.flips_needed > 0 else 0
+
+    def update(self, now):
+        if self.current_index == self.target_index:
+            return
+
+        if now - self.last_flip >= self.seconds_per_flip:
+            self.current_index = (self.current_index + 1) % len(self.ALPHABET)
+            self.last_flip = now
+
+    def char(self):
+        return self.ALPHABET[self.current_index]
+
+    def done(self):
+        return self.current_index == self.target_index
 
     
